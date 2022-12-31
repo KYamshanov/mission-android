eae27667fdd1ceed2cd0a78e89b14eb53eb8e2fb
Ямшанов Константин Алексеевич (4059153) <yamshanov@dev.vtb.ru>
2022-12-31 09:55:14 +0400

    Initial commit

---
+++ app/src/main/java/ru/kyamshanov/mission/android/MainActivity.kt
@@ -0,0 +1,40 @@
+package ru.kyamshanov.mission.android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.kyamshanov.mission.ui_core.ui.theme.MissionTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MissionTheme {
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MissionTheme {
+        Greeting("Android")
+    }
+}
